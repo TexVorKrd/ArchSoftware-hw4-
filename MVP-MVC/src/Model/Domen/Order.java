e60a8d40a4d2113b2304aab9c951684d4a2a2035
Sergey Vorotilin <sflash@mail.ru>
2023-08-26 23:11:27 +0300

    correction

--- MVP-MVC/src/Model/Domen/Order.java
+++ MVP-MVC/src/Model/Domen/Order.java
@@ -1,4 +1,19 @@
 package Model.Domen;
 
+import java.util.Date;
+
 public class Order {
+    int id;
+    Date date;
+    String name;
+    String phone;
+    Table table;
+
+    public Order(int id, Date date, String name, String phone, Table table) {
+        this.id = id;
+        this.date = date;
+        this.name = name;
+        this.phone = phone;
+        this.table = table;
+    }
 }
